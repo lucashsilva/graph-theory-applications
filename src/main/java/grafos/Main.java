3113f5c493e57f6fdd69b342141d2501900c2cf1
Lucas Silva <lucashenrique125@gmail.com>
2018-11-07 21:03:41 -0300

    Cleaning code

--- src/main/java/grafos/Main.java
+++ src/main/java/grafos/Main.java
@@ -28,30 +28,11 @@ public final class Main {
     	VertexProvider<Country> vertexProvider = new GraphVertexProvider();
     	EdgeProvider<Country, Edge> edgeProvider = new GraphEdgeProvider();
    
-    	CSVImporter<Country, Edge> importer = new CSVImporter(vertexProvider, edgeProvider);
+    	CSVImporter<Country, Edge> importer = new CSVImporter<Country, Edge>(vertexProvider, edgeProvider);
     	
     	File csv = new File("./src/assets/edges.csv");
     	
     	importer.importGraph(graph, csv);
-  
-        Graph<String, DefaultEdge> g = new SimpleGraph<>(DefaultEdge.class);
-
-        String v1 = "v1";
-        String v2 = "v2";
-        String v3 = "v3";
-        String v4 = "v4";
-
-        // add the vertices
-        g.addVertex(v1);
-        g.addVertex(v2);
-        g.addVertex(v3);
-        g.addVertex(v4);
-
-        // add edges to create a circuit
-        g.addEdge(v1, v2);
-        g.addEdge(v2, v3);
-        g.addEdge(v3, v4);
-        g.addEdge(v4, v1);
 
         return graph;
     }
