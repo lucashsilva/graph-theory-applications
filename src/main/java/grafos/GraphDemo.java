86e7e393e8813eaae98b7ce0e3e4c4bbc4643ee1
Lucas Silva <lucashenrique125@gmail.com>
2018-11-08 00:25:44 -0300

    Implemented visualization with reduced data and adjusted original matrix

---
+++ src/main/java/grafos/GraphDemo.java
@@ -0,0 +1,98 @@
+package grafos;
+
+import java.awt.Dimension;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.rmi.server.ExportException;
+
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+
+import org.jgrapht.ListenableGraph;
+import org.jgrapht.ext.JGraphXAdapter;
+import org.jgrapht.graph.DefaultListenableGraph;
+import org.jgrapht.graph.Pseudograph;
+import org.jgrapht.io.CSVFormat;
+import org.jgrapht.io.CSVImporter;
+import org.jgrapht.io.EdgeProvider;
+import org.jgrapht.io.ImportException;
+import org.jgrapht.io.VertexProvider;
+
+import com.mxgraph.layout.mxCircleLayout;
+import com.mxgraph.swing.mxGraphComponent;
+
+import grafos.models.Country;
+import grafos.models.Edge;
+import grafos.providers.GraphEdgeProvider;
+import grafos.providers.GraphVertexProvider;
+
+public final class GraphDemo extends JApplet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static final Dimension DEFAULT_SIZE = new Dimension(800, 600);
+	private JGraphXAdapter<Country, Edge> adapter;
+	
+    public static void main(String[] args) throws MalformedURLException, ExportException, org.jgrapht.io.ExportException, ImportException {
+        GraphDemo applet = new GraphDemo();
+        
+        applet.init();
+
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(applet);
+        frame.setTitle("JGraphT Adapter to JGraphX Demo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    @Override
+    public void init() {
+    	try {
+			ListenableGraph<Country, Edge> g = createGraph();
+			
+			adapter = new JGraphXAdapter(g);
+			
+			setPreferredSize(DEFAULT_SIZE);
+	        mxGraphComponent component = new mxGraphComponent(adapter);
+	        component.setConnectable(false);
+	        component.getGraph().setAllowDanglingEdges(false);
+	        getContentPane().add(component);
+	        resize(DEFAULT_SIZE);
+	        
+	        mxCircleLayout layout = new mxCircleLayout(adapter);
+
+	        // center the circle
+	        int radius = 100;
+	        layout.setX0((DEFAULT_SIZE.width / 2.0) - radius);
+	        layout.setY0((DEFAULT_SIZE.height / 2.0) - radius);
+	        layout.setRadius(radius);
+	        layout.setMoveCircle(true);
+
+	        layout.execute(adapter.getDefaultParent());
+		} catch (ImportException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+
+    private static ListenableGraph<Country, Edge> createGraph() throws ImportException {
+    	ListenableGraph<Country, Edge> graph = new DefaultListenableGraph(new Pseudograph<Country, Edge>(Edge.class));
+    	
+    	VertexProvider<Country> vertexProvider = new GraphVertexProvider();
+    	EdgeProvider<Country, Edge> edgeProvider = new GraphEdgeProvider();
+   
+    	CSVImporter<Country, Edge> importer = new CSVImporter<Country, Edge>(vertexProvider, edgeProvider);
+    	
+    	File csv = new File("./src/assets/adjacent_matrix_reduced.csv");
+    	
+    	importer.setFormat(CSVFormat.MATRIX);
+    	importer.setParameter(CSVFormat.Parameter.MATRIX_FORMAT_NODEID, true);
+    	importer.setParameter(CSVFormat.Parameter.MATRIX_FORMAT_EDGE_WEIGHTS, true);
+    	
+    	importer.importGraph(graph, csv);
+    	
+        return graph;
+    }
+}
--- src/main/java/grafos/models/Edge.java
+++ src/main/java/grafos/models/Edge.java
@@ -4,31 +4,77 @@ public class Edge {
 	private Country from;
 	private Country to;
 	private String label;
-	
+
 	public Edge(Country from, Country to, String label) {
 		super();
-		System.out.println(from.getId() + " " + to.getId() + "\n");
 		this.from = from;
 		this.to = to;
 		this.label = label;
 	}
-	
+
 	public Country getFrom() {
 		return from;
 	}
+
 	public void setFrom(Country from) {
 		this.from = from;
 	}
+
 	public Country getTo() {
 		return to;
 	}
+
 	public void setTo(Country to) {
 		this.to = to;
 	}
+
 	public String getLabel() {
 		return label;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((from == null) ? 0 : from.hashCode());
+		result = prime * result + ((label == null) ? 0 : label.hashCode());
+		result = prime * result + ((to == null) ? 0 : to.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Edge other = (Edge) obj;
+		if (from == null) {
+			if (other.from != null)
+				return false;
+		} else if (!from.equals(other.from))
+			return false;
+		if (label == null) {
+			if (other.label != null)
+				return false;
+		} else if (!label.equals(other.label))
+			return false;
+		if (to == null) {
+			if (other.to != null)
+				return false;
+		} else if (!to.equals(other.to))
+			return false;
+		return true;
+	}
+
 	public void setLabel(String label) {
 		this.label = label;
+	}
+
+	@Override
+	public String toString() {
+		return "Edge [label=" + label + "]";
 	}
 }
